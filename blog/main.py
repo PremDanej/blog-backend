780ae5cdbad031516d3a822b91c1580897204cde
PremDanej <danejprem005@gmail.com>
2025-06-05 18:49:06 +0530

    Fast API - Entrypoint of project.

---
+++ blog/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from connection.connection import engine
+from routers import (
+    user,
+    blog,
+)
+from schema.Base import Base
+
+Base.metadata.create_all(engine)
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return "Welcome to Blog"
+
+
+app.include_router(router=blog.router)
+app.include_router(router=user.router)
