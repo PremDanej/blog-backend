339000bce99725b3e718b3af977f7e9d3455f335
PremDanej <danejprem005@gmail.com>
2025-06-07 04:08:32 +0530

    Get data from env file.

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 import uvicorn
+from dotenv import load_dotenv
+import os
 
 if __name__ == "__main__":
+    load_dotenv()
+
+    host = os.getenv("HOST")
+    port = os.getenv("PORT")
+
     uvicorn.run(
         app="blog.main:app",
-        host="127.0.0.1",
-        port=8002,
+        host=host,
+        port=int(port),
         reload=True,
     )
